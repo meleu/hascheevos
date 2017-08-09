1ca13f8dd7d3ad8036a7ac57f787aa5831a94114
meleu <meleuzord@yahoo.com>
2017-08-09 04:22:56 -0300

    updated rom_has_cheevos.sh

--- bin/rom_has_cheevos.sh
+++ bin/rom_has_cheevos.sh
@@ -10,40 +10,55 @@
 
         
 
-# TODO: check dependencies curl, xmlstarlet, jq, md5sum, zcat, 7z,
-#       cheevoshash (from this repo).
+# TODO: check dependencies curl, jq, zcat, unzip, 7z, cheevoshash (from this repo).
 
-readonly USAGE="USAGE:
+readonly USAGE="
+USAGE:
 $0 romfile1 [romfile2 ...]"
 
 readonly GAMEID_REGEX='^[1-9][0-9]{1,9}$'
 
 # the extensions below was taken from RetroPie's configs
-EXTENSIONS='zip|7z|nes|fds|gb|gba|gbc|sms|bin|smd|gen|md|sg|smc|sfc|fig|swc|mgd|iso|cue|z64|n64|v64|pce|ccd|cue'
+readonly EXTENSIONS='zip|7z|nes|fds|gb|gba|gbc|sms|bin|smd|gen|md|sg|smc|sfc|fig|swc|mgd|iso|cue|z64|n64|v64|pce|ccd|cue'
 SCRIPT_DIR="$(dirname $0)"
 SCRIPT_DIR="$(cd "$SCRIPT_DIR" && pwd)"
-# TODO: DATA_DIR="$SCRIPT_DIR/data"
-DATA_DIR="$SCRIPT_DIR"
+readonly DATA_DIR="$SCRIPT_DIR/../data"
+
 RA_USER=
 RA_PASSWORD=
 RA_TOKEN=
+CHECK_FALSE_FLAG=0
+
+CONSOLE_NAME=()
+CONSOLE_NAME[1]=megadrive
+CONSOLE_NAME[2]=n64
+CONSOLE_NAME[3]=snes
+CONSOLE_NAME[4]=gb
+CONSOLE_NAME[5]=gba
+CONSOLE_NAME[6]=gbc
+CONSOLE_NAME[7]=nes
+CONSOLE_NAME[8]=pcengine
+CONSOLE_NAME[9]=segacd
+CONSOLE_NAME[10]=sega32x
+CONSOLE_NAME[11]=mastersystem
+#CONSOLE_NAME[12]=xbox360
+#CONSOLE_NAME[13]=atari
+#CONSOLE_NAME[14]=neogeo
+
 
 # Getting cheevos account info (needed to retrieve achievements list).
 function get_cheevos_token() {
-    local user="$RA_USER"
-    local password="$RA_PASSWORD"
-
-    if [[ -z "$user" ]]; then
+    if [[ -z "$RA_USER" ]]; then
         echo "ERROR: undefined RetroAchievements.org user (see \"--user\" option)." >&2
         exit 1
     fi
 
-    if [[ -z "$password" ]]; then
+    if [[ -z "$RA_PASSWORD" ]]; then
         echo "ERROR: undefined RetroAchievements.org password (see \"--password\" option)." >&2
         exit 1
     fi
 
-    RA_TOKEN="$(curl -s "http://retroachievements.org/dorequest.php?r=login&u=${USER}&p=${password}" | jq -r .Token)"
+    RA_TOKEN="$(curl -s "http://retroachievements.org/dorequest.php?r=login&u=${RA_USER}&p=${RA_PASSWORD}" | jq -r .Token)"
     if [[ "$RA_TOKEN" == null || -z "$RA_TOKEN" ]]; then
         echo "ERROR: cheevos authentication failed. Aborting..."
         exit 1
@@ -64,6 +79,38 @@ function get_cheevos_token() {
 }
 
 
+function download_hashlibrary() {
+    local console_id="$1"
+
+    if [[ "$console_id" -le 0 || "$console_id" -gt "${#CONSOLE_NAME[@]}" ]]; then
+        echo "ERROR: invalid console ID: $console_id" >&2
+        exit 1
+    fi
+
+    local json_file="$DATA_DIR/${CONSOLE_NAME[console_id]}_hashlibrary.json"
+
+    echo "--- getting the console hash library for \"${CONSOLE_NAME[console_id]}\"..." >&2
+    curl -s "http://retroachievements.org/dorequest.php?r=hashlibrary&c=$console_id" \
+        | jq '.' > "$json_file" 2> /dev/null \
+        || echo "ERROR: failed to download hash library for \"${CONSOLE_NAME[console_id]}\"!" >&2
+
+    [[ -s "$json_file" ]] || rm -f "$json_file"
+}
+
+
+function get_hash_libraries() {
+    local i
+
+    echo "Getting hash libraries..."
+
+    for i in $(seq 1 ${#CONSOLE_NAME[@]}); do
+        # XXX: do not get hashlibrary for sega32x and segacd (currently unsupported)
+        [[ $i -eq 9 || $i -eq 10 ]] && continue
+        download_hashlibrary "$i"
+    done
+}
+
+
 # print the game ID of a given rom file
 # XXX: DONE!
 function get_game_id() {
@@ -71,12 +118,13 @@ function get_game_id() {
     local hash
     local hash_i
     local gameid
+    local console_id=0
 
     hash="$(get_rom_hash "$rom")" || return 1
 
     for hash_i in $(echo "$hash" | sed 's/^\(SNES\|NES\|Genesis\|plain MD5\): //'); do
         echo "--- hash:    $hash_i" >&2
-        gameid="$(grep -h "\"$hash_i\"" *_hashlibrary.json | cut -d: -f2 | tr -d ' ,')"
+        gameid="$(grep -h "\"$hash_i\"" "$DATA_DIR"/*_hashlibrary.json 2> /dev/null | cut -d: -f2 | tr -d ' ,')"
         [[ $gameid =~ $GAMEID_REGEX ]] && break
     done
 
@@ -85,7 +133,14 @@ function get_game_id() {
         for hash_i in $(echo "$hash" | sed 's/^\(SNES\|NES\|Genesis\|plain MD5\): //'); do
             echo "--- hash:    $hash_i" >&2
             gameid="$(curl -s "http://retroachievements.org/dorequest.php?r=gameid&m=$hash_i" | jq .GameID)"
-            [[ $gameid =~ $GAMEID_REGEX ]] && break
+            if [[ $gameid =~ $GAMEID_REGEX ]]; then
+                # if the logic reaches this point, mark this game's console to download the hashlibrary
+                console_id="$(
+                    curl -s "http://retroachievements.org/dorequest.php?r=patch&u=${RA_USER}&g=${gameid}&f=3&l=1&t=${RA_TOKEN}" \
+                        | jq '.PatchData.ConsoleID'
+                )"
+                break
+            fi
         done
     fi
 
@@ -98,6 +153,11 @@ function get_game_id() {
         echo "ERROR: \"$rom\": unable to get game ID." >&2
         return 1
     fi
+
+    # if the logic reaches this point, we have a valid game ID
+
+    [[ "$console_id" -ne 0 ]] && download_hashlibrary "$console_id"
+
     echo "$gameid"
 }
 
@@ -120,28 +180,34 @@ function game_has_cheevos() {
     hascheevos_file="$(grep -l "^$gameid:" "$DATA_DIR"/*_hascheevos.txt)"
     if [[ -f "$hascheevos_file" ]]; then
         boolean="$(grep "^$gameid:" "$hascheevos_file" | cut -d: -f2)"
-        [[ "$boolean" == false ]] && return 1
-        [[ "$boolean" == true  ]] && return 0
+        [[ "$boolean" == true ]] && return 0
+        [[ "$boolean" == false && "$CHECK_FALSE_FLAG" -eq 0 ]] && return 1
     fi
     
     [[ -z "$RA_TOKEN" ]] && get_cheevos_token
 
     echo "--- checking at RetroAchievements.org server..." >&2
-    local number_of_cheevos="$(
-        curl -s "http://retroachievements.org/dorequest.php?r=patch&u=${USER}&g=${gameid}&f=3&l=1&t=${RA_TOKEN}" \
-        | jq '.PatchData.Achievements | length'
-    )"
+
+    local patch_json="$(curl -s "http://retroachievements.org/dorequest.php?r=patch&u=${RA_USER}&g=${gameid}&f=3&l=1&t=${RA_TOKEN}")"
+    local number_of_cheevos="$(echo "$patch_json" | jq '.PatchData.Achievements | length')"
     [[ -z "$number_of_cheevos" || "$number_of_cheevos" -lt 1 ]] && return 1
 
     # if the logic reaches this point, the game has cheevos
-    # XXX: update the _hascheevos.txt accordingly?
+
+    # updating the _hascheevos.txt file
+    local console_id="$(echo "$patch_json" | jq '.PatchData.ConsoleID')"
+    hascheevos_file="${CONSOLE_NAME[console_id]}_hascheevos.txt"
+
+    sed -i "s/^${gameid}:.*/${gameid}:true/" "$hascheevos_file"
+    grep -q "^${gameid}:true" "$hascheevos_file" || echo "${gameid}:true" >> "$hascheevos_file"
+    sort -un "$hascheevos_file" -o "$hascheevos_file"
+
     sleep 1 # XXX: a small delay to not stress the server
     return 0
 }
 
 
 # print the hash of a given rom file
-# DEP: cheevoshash, zcat, 7z
 function get_rom_hash() {
     local rom="$1"
     local hash
@@ -152,17 +218,17 @@ function get_rom_hash() {
         *.zip|*.ZIP)
             uncompressed_rom="$(unzip -o -d /tmp "$rom" | sed -e '/\/tmp/!d; s/.*inflating: //; s/ *$//')"
             validate_rom_file "$uncompressed_rom" || return 1
-            hash="$(./cheevoshash "$uncompressed_rom")"
+            hash="$($SCRIPT_DIR/cheevoshash "$uncompressed_rom")"
             rm -f "$uncompressed_rom"
             ;;
         *.7z|*.7Z)
             uncompressed_rom="/tmp/$(7z e -y -bd -o/tmp "$rom" | sed -e '/Extracting/!d; s/Extracting  //')"
             validate_rom_file "$uncompressed_rom" || return 1
-            hash="$(./cheevoshash "$uncompressed_rom")"
+            hash="$($SCRIPT_DIR/cheevoshash "$uncompressed_rom")"
             rm -f "$uncompressed_rom"
             ;;
         *)
-            hash="$(./cheevoshash "$rom")"
+            hash="$($SCRIPT_DIR/cheevoshash "$rom")"
             ;;
     esac
     [[ "$hash" =~ :\ [^\ ]{32} ]] || return 1
@@ -172,6 +238,12 @@ function get_rom_hash() {
 
 function validate_rom_file() {
     local rom="$1"
+
+    if [[ -z "$rom" ]]; then
+        echo "ERROR: missing ROM file name." >&2
+        echo "$USAGE" >&2
+        return 1
+    fi
 
     if [[ ! -f "$rom" ]]; then
         echo "ERROR: \"$rom\": file not found!" >&2
@@ -231,7 +303,7 @@ while [[ -n "$1" ]]; do
             echo
             # getting the help message from the comments in this source code
             sed -n 's/^#H //p' "$0"
-            exit 0
+            exit
             ;;
 
 #H -u|--user USER           USER is your RetroAchievements.org username.
@@ -250,6 +322,23 @@ while [[ -n "$1" ]]; do
             RA_PASSWORD="$1"
             ;;
 
+#H --get-hashlibs           Download JSON hash libraries for all supported
+#H                          consoles and exit.
+#H 
+        --get-hashlibs)
+            get_hash_libraries
+            exit
+            ;;
+
+#H --check-false            Check at RetroAchievements.org server even if the
+#H                          game ID is marked as "has no cheevos" (false) in the
+#H                          local *_hascheevos.txt files.
+#H 
+        --check-false)
+            CHECK_FALSE_FLAG=1
+            ;;
+
+# TODO: --repo-compare
         *)  break
             ;;
     esac
